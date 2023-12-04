7d9654cfe7c31c2ec11d919e3bbe9547abb6f974
tsai <lovez81427@gmail.com>
2023-12-04 13:45:23 +0800

    測試網頁切版,從首頁中間挖走一塊,並用include_once方式引入進來

--- news.php
+++ news.php
@@ -3,7 +3,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
 <title>卓越科技大學校園資訊系統</title>
-<link href="./news_files/css.css" rel="stylesheet" type="text/css">
+<link href="./css/css.css" rel="stylesheet" type="text/css">
 <script src="./css/jquery-1.9.1.min.js"></script>
 <script src="./js/js.js"></script>
 </head>
