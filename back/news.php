a9e5519ee017d5d6bcb2e797d4588a3b3100df5d
tsai <lovez81427@gmail.com>
2023-12-15 14:50:54 +0800

    增加次選單數計算及加強次選單空值檢查

--- back/news.php
+++ back/news.php
@@ -4,22 +4,23 @@
         <table width="100%" style="text-align: center">
             <tbody>
                 <tr class="yel">
-                    <td width="80%">最新消息資料</td>
+                    <td width="80%">最新消息資料內容</td>
                     <td width="10%">顯示</td>
                     <td width="10%">刪除</td>
                 </tr>
-                <?php    
+                <?php
                 $total=$DB->count();
                 $div=5;
                 $pages=ceil($total/$div);
                 $now=$_GET['p']??1;
                 $start=($now-1)*$div;
-                $rows=$DB->all("limit $start,$div");          
+                $rows=$DB->all(" limit $start,$div");
                 foreach($rows as $row){
                 ?>
                 <tr>
                     <td>
-                        <textarea type="text" name="text[<?=$row['id'];?>]" style="width:90%;height:60px"><?=$row['text'];?>"></textarea>
+                        <textarea type="text" name="text[]" style="width:90%;height:60px"><?=$row['text'];?></textarea>
+                        <input type="hidden" name="id[]" value="<?=$row['id'];?>">
                     </td>
                     <td>
                         <input type="checkbox" name="sh[]" value="<?=$row['id'];?>" <?=($row['sh']==1)?'checked':'';?>>
@@ -34,17 +35,15 @@
             </tbody>
         </table>
         <div class="cent">
-
+            
             <?php
-
                 if($now>1){
                     $prev=$now-1;
                     echo "<a href='?do=$do&p=$prev'> < </a>";
                 }
 
-                        
                 for($i=1;$i<=$pages;$i++){
-                    $fontsize=($now==$i)? '24px':'16px';
+                    $fontsize=($now==$i)?'24px':'16px';
                     echo "<a href='?do=$do&p=$i' style='font-size:$fontsize'> $i </a>";
                 }
 
@@ -58,7 +57,7 @@
             <tbody>
                 <tr>
                     <input type="hidden" name="table" value="<?=$do;?>">
-                    <td width="200px"><input type="button" onclick="op('#cover','#cvr','./modal/<?=$do;?>.php?table=<?=$do;?>')" value="新增最新消息"></td>
+                    <td width="200px"><input type="button" onclick="op('#cover','#cvr','./modal/<?=$do;?>.php?table=<?=$do;?>')" value="新增最新消息資料"></td>
                     <td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置"></td>
                 </tr>
             </tbody>
